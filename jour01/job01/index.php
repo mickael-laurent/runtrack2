9cfba7eb1b761895b572d863642964b8c5e2dad8
mickael laurent <mickaellaurent@gmx.fr>
2023-11-14 16:47:58 +0100

    ajout de t3 jobs terminer

---
+++ jour01/job01/index.php
@@ -0,0 +1,14 @@
+<?php
+$my_str = "La plateforme";
+$my_str2 = "Vive";
+$my_str3 = "!";
+$val = "6";
+$myBool = "true";
+echo "<p>La valeur de my str est : $my_str </p>";
+echo "<p>La valeur de my string est : $my_str2 </p>";
+echo "<p>La valeur de my string est : $my_str3 </p>";
+echo "<p>La valeur de my string est : $val </p>";
+echo "<p>La valeur de my string est : $myBool </p>";
+?>
+
+
---
+++ jour01/job02/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>Hello La plateforme </p>";
+echo '<p>Hello La plateforme </p>';
