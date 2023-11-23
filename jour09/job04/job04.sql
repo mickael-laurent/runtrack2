f4aa6b41ad957ed06ad9d4fcdde1e2c979532c69
mickael laurent <mickaellaurent@gmx.fr>
2023-11-23 16:40:25 +0100

    ajout du jour 9 non terminer
    jai compris le principale des job ajouter

---
+++ jour09/job04/job04.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : jeu. 23 nov. 2023 à 12:38
+-- Version du serveur : 8.0.30
+-- Version de PHP : 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `jour09`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etage`
+--
+
+CREATE TABLE `etage` (
+  `id` int NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `numero` int NOT NULL,
+  `superficie` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `etage`
+--
+
+INSERT INTO `etage` (`id`, `nom`, `numero`, `superficie`) VALUES
+(1, 'RDC', 0, 500),
+(2, 'R+1', 1, 500);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etudiant`
+--
+
+CREATE TABLE `etudiant` (
+  `id` int NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `naissance` date NOT NULL,
+  `sexe` varchar(25) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `etudiant`
+--
+
+INSERT INTO `etudiant` (`id`, `prenom`, `nom`, `naissance`, `sexe`, `email`) VALUES
+(1, 'Cyril', 'Zimmermann', '1989-01-02', 'Homme', 'cyril@laplateforme.io'),
+(2, 'Jessica', 'Soriano', '1995-09-08', 'Femme', 'jessica@laplateforme.io'),
+(3, 'Roxan', 'Roumégas', '2016-09-08', 'Femme', 'roxan@laplateforme.io'),
+(4, 'Pascal', 'Assens', '1999-12-31', 'Homme', 'pascal@laplateforme.io'),
+(5, 'Terry', 'Cristinelli', '2005-02-01', 'Homme', 'terry@laplateforme.io'),
+(6, 'Ruben', 'Habib', '1993-05-26', 'Homme', 'ruben.habib@laplateforme.io'),
+(7, 'Toto', 'Dupont', '2019-11-07', 'Homme', 'toto@laplateforme.io');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `salle`
+--
+
+CREATE TABLE `salle` (
+  `id` int NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `id_etage` int NOT NULL,
+  `capacite` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `salle`
+--
+
+INSERT INTO `salle` (`id`, `nom`, `id_etage`, `capacite`) VALUES
+(1, 'Lounge', 1, 100),
+(2, 'Studio Son', 1, 5),
+(3, 'Brocasting', 2, 50),
+(4, 'Local Peda', 2, 4),
+(5, 'Coworking', 2, 80),
+(6, 'Studio Video', 2, 5);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `etage`
+--
+ALTER TABLE `etage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `etudiant`
+--
+ALTER TABLE `etudiant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `salle`
+--
+ALTER TABLE `salle`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `etage`
+--
+ALTER TABLE `etage`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `etudiant`
+--
+ALTER TABLE `etudiant`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `salle`
+--
+ALTER TABLE `salle`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+SELECT nom, capacite FROM salle;
