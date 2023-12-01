b8e85af7ecad3f1579c850963db6922be6a23c4b
mickael laurent <mickaellaurent@gmx.fr>
2023-12-01 10:06:33 +0100

    ajout du jour 10

---
+++ jour10/job11/index.php
@@ -0,0 +1,49 @@
+<?php
+// Informations de connexion à la base de données
+$serveur = "localhost"; // Serveur de la base de données
+$utilisateur = "root"; // Nom d'utilisateur de la base de données
+$motDePasse = ""; // Mot de passe de la base de données
+$nomBaseDeDonnees = "jour09"; // Nom de la base de données
+
+// Connexion à la base de données
+$mysqli = new mysqli($serveur, $utilisateur, $motDePasse, $nomBaseDeDonnees);
+
+// Vérification de la connexion
+if ($mysqli->connect_errno) {
+    echo "Echec de la connexion à la base de données : " . $mysqli->connect_error;
+    exit();
+}
+
+// Requête SQL pour récupérer toutes les informations de la table 'etudiants'
+$requete = "SELECT AVG(capacite) AS capacite_moyenne FROM salle";
+$resultat = $mysqli->query($requete);
+
+if ($resultat) {
+    echo "<table border='1'>";
+    echo "<thead><tr>";
+    // Affichage des noms des colonnes
+    while ($info_colonne = $resultat->fetch_field()) {
+        echo "<th>".$info_colonne->name."</th>";
+    }
+    echo "</tr></thead>";
+    echo "<tbody>";
+    // Affichage des données
+    while ($ligne = $resultat->fetch_assoc()) {
+        echo "<tr>";
+        foreach ($ligne as $valeur) {
+            echo "<td>".$valeur."</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+
+    // Libération du résultat
+    $resultat->free();
+} else {
+    echo "Erreur dans la requête : " . $mysqli->error;
+}
+
+// Fermeture de la connexion
+$mysqli->close();
+?>
